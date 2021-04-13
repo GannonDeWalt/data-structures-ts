ffb6c410f70ff1a4c386fbf10c039fefa6fa511d
GannonDeW <>
2021-04-13 19:46:25 +0000

    Fix Queue.ts

--- src/Queue.ts
+++ src/Queue.ts
@@ -1,1 +1,33 @@
-export default class Queue {}
+import Node from "./Node";
+export default class Queue {
+
+  private nodes : Node[]
+
+  constructor(){
+    this.nodes = [];
+  }
+
+  isEmpty(){
+    return this.nodes.length == 0;
+  }
+
+  enqueue(node : Node){
+    this.nodes.push(node);
+  }
+
+  length(){
+    return this.nodes.length;
+  }
+
+  front(){
+    return this.nodes[0];
+  }
+
+  rear(){
+    return this.nodes[this.nodes.length - 1];
+  }
+
+  dequeue(){
+    return this.nodes.shift();
+  }
+}
