88fee5e758c227bdfc58b51d4d6fa6554e63051e
GannonDeW <>
2021-04-13 19:22:12 +0000

    Attempt LinkedList

--- src/LinkedList.ts
+++ src/LinkedList.ts
@@ -1,5 +1,71 @@
+import Node from "./Node";
 export default class LinkedList {
 
-  
+  private size: number;
+  public tail : Node | undefined;
+  public head : Node | undefined;
+
+  constructor(){
+    this.head = undefined;
+    this.tail = undefined;
+    this.size = 0;
+
+  }
+
+
+  add(node : Node){
+    if(!this.head) //if head does not exist
+      this.head = node;
+
+    this.tail.next = node;
+    node.next = {} as Node;
+    this.tail = node;
+
+    this.size++;
+  }
+
+  remove(node : Node){
+
+    if (node === this.head){ //if deleting head
+      if(node.next)
+        this.head = node.next;
+      else
+        this.head = undefined;
+      this.tail = undefined;
+    }
+
+    var temp;
+    for (var current = this.head; current; current = current.next){
+      if(current.value === node.value){
+        break;
+      }//end if
+      temp = current;
+    }//end for
+
+    if(!node.next){// if deleting the tail
+      temp.next = undefined;
+      this.tail = temp
+    }
+    else{ //if deleting neither head or tail
+      temp.next = node.next;
+    }
+
+
+
+    
+  }
+
+  count(){
+    return this.size;
+  }
+
+  find(value : any){
+    for (var current = this.head; current; current = current.next){
+      if(current.value === value){
+        return current;
+      }//end if
+    }//end for
+    return undefined;
+  }
 
 }
--- tests/LinkedList.test.ts
+++ tests/LinkedList.test.ts
@@ -13,11 +13,11 @@ describe("LinkedList", () => {
         linkedList = new LinkedList();
     })
 
-    // test("should return head node", () => {
-    //     let node = new Node();
-    //     linkedList.add(node)
-    //     expect(linkedList.head).toBeTruthy()
-    // });
+    test("should return head node", () => {
+        let node = new Node();
+        linkedList.add(node)
+        expect(linkedList.head).toBeTruthy()
+    });
 
     test("should add new node to end of the list", () => {
         let aNode = new Node();
@@ -28,20 +28,20 @@ describe("LinkedList", () => {
         expect(aNode.next).toEqual(bNode);
     })
 
-    // test("should return total count of nodes", () => {
-    //     let node = new Node();
-    //     linkedList.add(node)
-    //     expect(linkedList.count()).toEqual(1)
-    // });
+    test("should return total count of nodes", () => {
+        let node = new Node();
+        linkedList.add(node)
+        expect(linkedList.count()).toEqual(1)
+    });
 
-    // test("should find a node and return it", () => {
-    //     let node = new Node("Shoe");
-    //     let node2 = new Node("Pencil");
-    //     linkedList.add(node)
-    //     linkedList.add(node2)
-    //     let result = linkedList.find("Shoe");
-    //     expect(result).toEqual(node);
-    // });
+    test("should find a node and return it", () => {
+        let node = new Node("Shoe");
+        let node2 = new Node("Pencil");
+        linkedList.add(node)
+        linkedList.add(node2)
+        let result = linkedList.find("Shoe");
+        expect(result).toEqual(node);
+    });
 
     // test("should remove a node", () => {
     //     let aNode = new Node();
